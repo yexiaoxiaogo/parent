2ea1784b8f06f80ad70c54136ef7f1003326cceb
yexiaoxiaogo <841079936@qq.com>
2018-06-13 16:47:20 +0800

    创建一个核心多模块框架

---
+++ partent-microservice/aop/src/main/java/com/github/yexiaoxiaogo/aop/App.java
@@ -0,0 +1,13 @@
+package com.github.yexiaoxiaogo.aop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
